ba575bdd529983c7cb40d12251c610e66c112f57
Camilo Andres Hurtado Carvajal <camilo.hurtado@globant.com>
2020-10-08 00:02:18 -0500

    Refactor: unused imports removed

--- src/main/java/com/optivision/webapp/commons/GlobalExceptionHandler.java
+++ src/main/java/com/optivision/webapp/commons/GlobalExceptionHandler.java
@@ -8,10 +8,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import org.springframework.web.util.WebUtils;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -19,7 +15,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler({CollidingAppointmentHoursException.class})
     public final ResponseEntity<Object> handleException(CollidingAppointmentHoursException ex, WebRequest request) {
         HttpHeaders headers = new HttpHeaders();
-        //String bodyOfResponse = "This should be application specific";
         return handleExceptionInternal(ex, ex.getLocalizedMessage(),
                 new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
