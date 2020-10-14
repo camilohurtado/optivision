8458eca40ef18053ac98e237cabf5b0bb8548027
Camilo Andres Hurtado Carvajal <camilo.hurtado@globant.com>
2020-10-13 21:37:13 -0500

    Fix: some data types fixed in repository

--- src/main/java/com/optivision/webapp/sales/persistence/SalesRepository.java
+++ src/main/java/com/optivision/webapp/sales/persistence/SalesRepository.java
@@ -6,12 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface SalesRepository extends JpaRepository<SaleEntity, Long> {
 
-    public List<SaleEntity> getAllByFechaCreacionIs(LocalDate specificDate);
+    public List<SaleEntity> getAllByFechaCreacionIs(Date specificDate);
     public SaleEntity getByEstadoVentaIs(String estadoVenta);
-    public List<SaleEntity> getAllByFechaCreacionIs(LocalDate initialDate, LocalDate endDate);
+    //public List<SaleEntity> getAllByFechaCreacionIs(LocalDate initialDate, LocalDate endDate);
 }
