47057e1bda8a419b13d640116652a454f7556051
Camilo Andres Hurtado Carvajal <camilo.hurtado@globant.com>
2020-10-20 01:10:36 -0500

    Add: new Exception added

--- src/main/java/com/optivision/webapp/product/exception/InvalidMaterialException.java
+++ src/main/java/com/optivision/webapp/product/exception/InvalidMaterialException.java
@@ -1,2 +1,12 @@
-package com.optivision.webapp.product.exception;public class InvalidMaterialException {
+package com.optivision.webapp.product.exception;
+
+public class InvalidMaterialException extends RuntimeException{
+
+    public InvalidMaterialException(String message) {
+        super(message);
+    }
+
+    public InvalidMaterialException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
